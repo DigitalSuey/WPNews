4a59ce5e16963126975d3cbd3aa75747e4112828
Paulo Pão <paulopao@Paulos-Mac.BerNet>
2015-05-27 21:44:58 +1000

    Add project

---
+++ WestpacTest/WestpacTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WestpacTest
+//
+//  Created by Paulo Pão on 27/05/15.
+//  Copyright (c) 2015 Paulo Pão. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WestpacTest/WestpacTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WestpacTest
+//
+//  Created by Paulo Pão on 27/05/15.
+//  Copyright (c) 2015 Paulo Pão. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
